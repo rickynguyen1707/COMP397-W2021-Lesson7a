4ba31819ca79be6827f234e927c6dfabbceeba8b
Ricky <nguyenkhanglhp1605@gmail.com>
2021-03-06 23:07:51 -0500

    added sound effects

--- Assets/_Scripts/CryptoBehaviour.cs
+++ Assets/_Scripts/CryptoBehaviour.cs
@@ -8,50 +8,74 @@ public enum CryptoState
 {
     IDLE,
     RUN,
-    JUMP
+    JUMP,
+    KICK
 }
 
 
 public class CryptoBehaviour : MonoBehaviour
 {
     [Header("Line of Sight")] 
-    public bool HasLOS;
+    public bool HasLOS = false;
 
     public GameObject player;
 
     private NavMeshAgent agent;
     private Animator animator;
 
+    [Header("Attack")]
+    public float attackDistance;
+    public PlayerBehaviour playerBehaviour;
+    public float damageDelay = 1.0f;
+    public bool isAttacking = false;
+    public float kickForce = 0.01f;
+    public float distanceToPlayer;
+
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
+        playerBehaviour = FindObjectOfType<PlayerBehaviour>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        
+
         if (HasLOS)
         {
             agent.SetDestination(player.transform.position);
+            distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
         }
 
 
-        if(HasLOS && Vector3.Distance(transform.position, player.transform.position) < 2.5)
+        if(HasLOS && distanceToPlayer < attackDistance && !isAttacking)
         {
                 // could be an attack
-            animator.SetInteger("AnimState", (int)CryptoState.IDLE);
+            animator.SetInteger("AnimState", (int)CryptoState.KICK);
             transform.LookAt(transform.position - player.transform.forward);
+
+            
+            DoKickDamage();
+            isAttacking = true;
+            
 
             if (agent.isOnOffMeshLink)
             {
                 animator.SetInteger("AnimState", (int)CryptoState.JUMP);
             }
         }
-        else
+        else if (HasLOS && distanceToPlayer > attackDistance)
         {
             animator.SetInteger("AnimState", (int)CryptoState.RUN);
+            isAttacking = false;
+   
+        }
+        else
+        {
+            animator.SetInteger("AnimState", (int)CryptoState.IDLE);
         }
     }
 
@@ -64,5 +88,20 @@ public class CryptoBehaviour : MonoBehaviour
         }
     }
 
+    private void DoKickDamage()
+    {
+        playerBehaviour.TakeDamage(20);
+        StartCoroutine(kickBack());
+             
+    }
+
+    private IEnumerator kickBack()
+    {
+        yield return new WaitForSeconds(damageDelay);
+        
+        var direction = Vector3.Normalize(player.transform.position - transform.position);
+        playerBehaviour.controller.SimpleMove(direction * kickForce);
+        StartCoroutine(kickBack());
+    }
 }
 
